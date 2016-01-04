eebf7ab3e5d6be8504938fa22a1a8965c490dba2
Martin Ryan <martinrryan@gmail.com>
2016-01-04 16:57:56 -0600

    project Moogle initialized

---
+++ db/database.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+
+var connectionString = "mongodb://localhost/Moogle";
+
+mongoose.connect(connectionString);
+
+mongoose.connection.on("connected", function(){
+
+  console.log("Mongoose connected to " + connectionString);
+
+});
+
+mongoose.connection.on("error", function(error){
+
+  console.log("Mongoose has returned with error: " + error);
+
+});
+
+mongoose.connection.on("disconnected", function(){
+
+  console.log("Mongoose has disconnected from the database.");
+
+});
