175d2ca890cbd5378bc58da305ce41e1c6486517
Jeff Steed <steed.jeff@gmail.com>
2016-01-06 09:25:10 -0600

    Rearranged the order of loading for the backbone scripts

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,29 +1,6 @@
 var app = app || {};
 var active = active || {};
 
-app.Model = Backbone.Model.extend({
-
-});
-
-app.Collection = Backbone.Collection.extend({
-  model: app.Model,
-  url: "/location",
-  initialize: function() {
-    console.log("collection extender has been instantiated");
-    var self = this;
-    this.on("sync", function() {
-      var view = new app.CollectionView({
-        collection: self
-      });
-      console.log(view);
-    });
-    this.fetch();
-  }
-});
-
-Backbone.Model.idAttributes = "_id";
-
-
 $(document).ready(function() {
   active.collection = new app.Collection();
 });
